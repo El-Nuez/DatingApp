dd50d5e6d8294a1746b192464d61caf9a2a87d6b
El-Nuez <guitarra.cesar@hotmail.com>
2024-10-14 19:51:25 -0600

    Parcial02 - Update code with editor  config

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,21 +1,18 @@
+namespace API.Controllers;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Services;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
-namespace API.Controllers;
 
 
 public class AccountController(
     DataContext context,
     ITokenService tokenService) : BaseApiController
 {
-    
     [HttpPost("register")]
     public async Task<ActionResult<UserResponse>> RegisterAsync(RegisterRequest request)
     {
@@ -49,16 +46,21 @@ public class AccountController(
             x.UserName.ToLower() == request.Username.ToLower());
 
         if (user == null)
+        {
             return Unauthorized("Invalid username or password");
-        
+        }
 
         using var hmac = new HMACSHA512(user.PasswordSalt);
         var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
 
-        for (int i = 0; i < computeHash.Length; i++)
+        for (var i = 0; i < computeHash.Length; i++)
+        {
             if (computeHash[i] != user.PasswordHash[i])
+            {
                 return Unauthorized("Invalid username or password");
-                
+            }
+        }
+
         return new UserResponse
         {
             Username = user.UserName,
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,10 @@
+namespace API.Controllers;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
-namespace API.Controllers;
 
 [Authorize]
 public class UsersController : BaseApiController
@@ -32,14 +31,14 @@ public class UsersController : BaseApiController
     {
         var user = await _context.Users.FindAsync(id);
 
-        if (user == null) return NotFound();
+        if (user == null)
+        {
+            return NotFound();
+        }
 
         return user;
     }
 
     [HttpGet("{name}")] // api/v1/users/Calamardo
-    public ActionResult<string> Ready(string name)
-    {
-        return $"Hi {name}";
-    }
+    public ActionResult<string> Ready(string name) => $"Hi {name}";
 }
