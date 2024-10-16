a91460417978d9829ea59c8f8a70881dd3f2474d
El-Nuez <guitarra.cesar@hotmail.com>
2024-10-15 22:05:54 -0600

    Parcial02 -  Add server-error handling

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -1,4 +1,3 @@
-import { state } from '@angular/animations';
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
@@ -7,29 +6,29 @@ import { catchError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
-  const toastr = inject(ToastrService)
-
+  const toastr = inject(ToastrService);
+  
   return next(req).pipe(
     catchError(error => {
-      if (error){
+      if (error) {
         switch(error.status) {
           case 400:
-            if (error.error.errors){
+            if (error.error.errors) {
               const modalStateErrors = [];
-              for(const key in error.error.errors) {
-                if (error.error.errors[key]){
-                  modalStateErrors.push(error.error.errors[key]);
+              for (const key in error.error.errors) {
+                if (error.error.errors[key]) {
+                  modalStateErrors.push(error.error.errors[key])
                 }
               }
               throw modalStateErrors.flat();
-            }else{
+            } else {
               toastr.error(error.error, error.status);
             }
             break;
           case 401:
             toastr.error("Unauthorized", error.status);
             break;
-          case 402:
+          case 404:
             router.navigateByUrl("/not-found");
             break;
           case 500:
@@ -43,7 +42,7 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
       }
       throw error;
     })
-  )
+  );
 
   return next(req);
 };
