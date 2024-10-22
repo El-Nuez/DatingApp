fe673f8b34c4ca9d97df331f3910d8ce5612cc64
El-Nuez <guitarra.cesar@hotmail.com>
2024-10-22 09:53:30 -0600

    Parcial02 - Configure AutoMapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,12 +1,16 @@
 namespace API.Helpers;
+
 using API.DataEntities;
 using API.DTOs;
 using AutoMapper;
+
 public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
     {
-        CreateMap<AppUser, MemberResponse>();
+        CreateMap<AppUser, MemberResponse>()
+            .ForMember(d => d.PhotoUrl,
+                o => o.MapFrom(s => s.Photos.FirstOrDefault(p => p.IsMain)!.Url));
         CreateMap<Photo, PhotoResponse>();
     }
 }
