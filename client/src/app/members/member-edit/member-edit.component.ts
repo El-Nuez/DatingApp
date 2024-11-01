48f7d2abb634fc2da41d57642ae8778fce718b4a
El-Nuez <guitarra.cesar@hotmail.com>
2024-11-01 09:07:38 -0600

    Parcial03 - Add update profile

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,24 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
   standalone: true,
-  imports: [TabsModule,FormsModule],
+  imports: [TabsModule, FormsModule],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css'
 })
-export class MemberEditComponent {
+export class MemberEditComponent implements OnInit {
+  @ViewChild("editForm") editForm?: NgForm;
   member?: Member;
   private accountService = inject(AccountService);
   private membersService = inject(MembersService);
+  private toastr = inject(ToastrService);
+
   ngOnInit(): void {
     this.loadMember();
   }
+
   loadMember() {
     const user = this.accountService.currentUser();
     if (!user) return;
@@ -26,5 +31,10 @@ export class MemberEditComponent {
       next: member => this.member = member
     })
   }
-  
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success("Profile updated!");
+    this.editForm?.reset(this.member);
+  }
 }
