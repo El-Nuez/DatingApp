24a48e38bbd1f2f86e347ed3dc28a340cdca29c6
El-Nuez <92186020+El-Nuez@users.noreply.github.com>
2025-02-05 00:42:08 -0600

    Develop (#6)
    * Parcial01 (#3)
    
    Squash and Merge:
    
    * Parcial01-Initial configuration
    
    * Parcial01 - First Entity
    
    * Parcial01 - Add EntityFramework Core
    
    * Parcial01- Add DataContext
    
    * Parcial01 -Add first EF migration
    
    * Parcial01 - Add database using dontnet ef database command
    
    * Parcial01 - Add Users controller
    
    * Parcial01 -Add Async code
    
    * Parcial01- Initial Angular commit
    
    * Parcial01- Add first Angular component
    
    * Parcial01 - Add CORS
    
    * Parcial01 - Add users list to html
    
    * Parcial01 - Add ngx-bootstrap@12 bootstrap font-awesome
    
    * Parcial01- Create mkcert and add SSL
    
    * Parcial01 - Add password to appuser
    
    * Parcial01 - Add ApiControllerBase
    
    * Parcial01 -Add ApiControllerBase_v2
    
    * Parcial01 - Add RegisterAsync
    
    * Parcial01-Add DTOs
    
    * Parcial01 - Add login endpoint
    
    * Parcial01 - Add Token service
    
    * Parcial01 - Add Token service
    
    * Parcial01 - Add authn middleware
    
    Parcial01 - Add authn middleware
    
    * Parcial01 - Add extension methods
    
    * Parcial01- Add nav bar to Angular SPA
    
    * Parcial01- Angular Template forms
    
    * Parcial01- Inject account service into component
    
    * Parcial01- Add conditionals to show/remove content from DOM
    
    * Parcial01 - Add Bootstrap dropdown
    
    * Parcial01 - Persist the login
    
    * Parcial01 - Add home page
    
    * Parcial01 - Add register component
    
    * Parcial01 - PARENT to CHILD communication
    
    * Parcial01 - CHILD to PARENT communication
    
    * Parcial01 - Add user registration
    
    * Parcial02 (#4)
    
    * Parcial02 - Add editorconfig
    
    * Parcial02 - Add routing and components
    
    * Parcial02 - Add nav links
    
    * Parcial02 - Add routing in code
    
    * Parcial02 - Add notification service
    
    * Parcial02 - Add CanActivate guard
    
    * Parcial02 - Add dummy route
    
    * Parcial02 - Add Bootswatch theme
    
    * Parcial02 - Update code with editor  config
    
    * Parcial02 - Add buggy controller to test errors
    
    * Parcial02 - Add Exception MIDDLEWARE
    
    * Parcial02 - Add test for errors from SPA
    
    * Parcial02 - Add error interceptor in SPA
    
    * Parcial02 - Add validation error section
    
    * Parcial02 - Add not-found and server-error landing pages
    
    * Parcial02 -  Add server-error handling
    
    * Parcial02 - Add new props to AppUser and create DateTime extension method
    
    * Parcial02 - Add EF Relationships
    
    * Parcial02 - Seed data
    
    * Parcial02 - Add REPOSITORY PATTERN
    
    * Parcial02 - Update users controller and add MemberResponse
    
    * Parcial02 - Add AutoMapper
    
    * Parcial02 - Add AutoMapper to UserController
    
    * Parcial02 - Configure AutoMapper
    
    * Parcial02 - AutoMapper queryable extensions
    Beta
    Give feedback
    
    * Parcial03 (#5)
    
    * Parcial03 - Add members support
    
    * Parcial03 - Add member card component
    
    * Parcial03 - Add style to member cards
    
    * Parcial03 - Add style to cards
    
    * Parcial03 - Add JWT Interceptor
    
    * Parcial03 - Add routing to member detail page
    
    * Parcial03 - Add style to member detail component
    
    * Parcial03 - Add photo gallery
    
    * Parcial03 - Execute npm audit fix
    
    * Parcial03 - Add member edit component
    
    * Parcial03 - Update member edit component
    
    * Parcial03 - Add update profile
    
    * Parcial03 - Add CanDeactivate route guard
    
    * Parcial03 - Update user repository
    
    * Parcial03 - Update member update in SPA
    
    * Parcial03 - Add loading indicator
    
    * Parcial03 - Add data recovery from app state (cache)
    
    * Parcial03 - Fix to ngx spinner type
    
    * Parcial03 - Add UNIT TESTS
    
    * Parcial03 - Update gitignore
    
    * Parcial03 - Update buggy controller tests
    
    * Parcial03 - Update tests
    
    * Parcial03 - Update tests with 89% coverage

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RegisterComponent } from "../register/register.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  registerMode = false;
+
+  registerToggle(): void {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean): void {
+    this.registerMode = event;
+  }
+
+  
+}
