7134ac817919b2dfa1a049942f27bdb202584a88
El-Nuez <guitarra.cesar@hotmail.com>
2025-02-08 22:31:17 -0600

    Parcial04 - Add PhotoService

--- API/Data/IUserRepository.cs
+++ API/Data/IUserRepository.cs
@@ -6,12 +6,10 @@ using API.DTOs;
 public interface IUserRepository
 {
     void Update(AppUser user);
-
     Task<bool> SaveAllAsync();
     Task<IEnumerable<AppUser>> GetAllAsync();
     Task<AppUser?> GetByIdAsync(int id);
     Task<AppUser?> GetByUsernameAsync(string username);
     Task<IEnumerable<MemberResponse>> GetMembersAsync();
     Task<MemberResponse?> GetMemberAsync(string username);
-
 }
