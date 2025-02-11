580d7b7b272e8c3516e87b9df6142e419a0ccbf5
El-Nuez <guitarra.cesar@hotmail.com>
2025-02-10 19:46:32 -0600

    Parcial014 - Add photo to nav bar

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,8 +44,9 @@ public class AccountController(
     [HttpPost("login")]
     public async Task<ActionResult<UserResponse>> LoginAsync(LoginRequest request)
     {
-        var user = await context.Users.FirstOrDefaultAsync(x =>
-            x.UserName.ToLower() == request.Username.ToLower());
+        var user = await context.Users
+            .Include(x => x.Photos)
+            .FirstOrDefaultAsync(x => x.UserName.ToLower() == request.Username.ToLower());
 
         if (user == null)
         {
@@ -66,7 +67,8 @@ public class AccountController(
         return new UserResponse
         {
             Username = user.UserName,
-            Token = tokenService.CreateToken(user)
+            Token = tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain)?.Url
         };
     }
 
