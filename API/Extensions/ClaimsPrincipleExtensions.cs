37c9bcf19a7b8607ccfa810e0ced85500b54167d
El-Nuez <guitarra.cesar@hotmail.com>
2025-03-09 23:52:01 -0600

    Parcial05 - Add TimeAgo module for date formatting

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -2,12 +2,14 @@ namespace API.Extensions;
 
 using System.Globalization;
 using System.Security.Claims;
+
 public static class ClaimsPrincipleExtensions
 {
     public static int GetUserId(this ClaimsPrincipal user)
     {
         var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier)
             ?? throw new ArgumentException("Cannot get the userId from token"), CultureInfo.InvariantCulture);
+
         return userId;
     }
 
@@ -15,6 +17,7 @@ public static class ClaimsPrincipleExtensions
     {
         var username = user.FindFirstValue(ClaimTypes.Name)
             ?? throw new ArgumentException("Cannot get the username from token");
+
         return username;
     }
 }
