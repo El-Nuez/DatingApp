d47baafabef0c533bd819396f844e36630c1747b
El-Nuez <guitarra.cesar@hotmail.com>
2025-04-05 19:54:33 -0600

    Parcial05 - Send messages

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -26,4 +26,8 @@ import { inject, Injectable, signal } from '@angular/core';
    getMessageThread(username: string) {
     return this.http.get<Message[]>(this.baseUrl + "messages/thread/" + username);
   }
+
+  sendMessage(username: string, content: string) {
+    return this.http.post<Message>(this.baseUrl + "messages", { recipientUsername: username, content });
+  }
  }
